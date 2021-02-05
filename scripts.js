1434b001a8499bc6ebb2b25e106b016e54f9000c
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-02-05 23:28:56 +0600

    added ul in body and added buttons

--- scripts.js
+++ scripts.js
@@ -8,3 +8,16 @@ function printLine(text) {
 let h1 = document.createElement('h1');
 h1.innerText = "0";
 document.body.append(h1);
+
+let ul = document.createElement('ul');
+for (let i = 0; i <= 5; i++) {
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "Example";
+    button.id = "example" + i;
+
+    li.append(button);
+    ul.append(li);
+}
+
+document.body.append(ul);
